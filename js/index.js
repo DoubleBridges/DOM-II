966082636ded63637bf9d3209ad2704101a15006
Dave Kane <davelkane@gmail.com>
2019-06-08 16:32:38 -0500

    Background color changes on resize

--- js/index.js
+++ js/index.js
@@ -1,25 +1,37 @@
 // Your code goes here
+
+//Nav changes color when mouse enters and returns to default on leave
 const headerNav = document.querySelector('.main-navigation');
 
 headerNav.addEventListener('mouseenter', (event) => event.target.style.backgroundColor = '#FFEBCD');
 
 headerNav.addEventListener('mouseleave', (event) => event.target.style.backgroundColor = '');
 
+//Make img's disappear on click and reapper on click anywhere other than img
 const images = document.querySelectorAll('img');
 
 const page = document.querySelector('body')
 
 page.addEventListener('click', event => {
-    images.forEach(function(el) {
-        if (el.style.visibility === 'hidden') {
-            event.target.style.visibility = 'visible'
-            console.log(el)
+    images.forEach(function (el) {
+        if (event.target.tagName !== 'IMG') {
+            el.style.visibility = 'visible'
         }
     })
 })
 
 images.forEach((el) => el.addEventListener('click', event => event.target.style.visibility = 'hidden'));
 
+// assign a random color to the background any time the window is resized
+const { innerWidth } = window;
+
+const colors = ['red', 'blue', 'green', 'yellow', 'orange', 'pink', 'darkorchid', 'teal', 'dodgerblue', 'crimson', 'orchid'];
+
+const randomColor = arr => {
+    page.style.backgroundColor = arr[Math.floor((Math.random() * 10) + 1)]
+}
+
+window.addEventListener('resize', () => randomColor(colors));
 
 
 
@@ -27,9 +39,3 @@ images.forEach((el) => el.addEventListener('click', event => event.target.style.
 
 
 
-
-
-// images.forEach(el => {
-    // if(el.visibility === 'hidden') {
-        // event => event.target.style.visibility = 'visible'; 
-    // });
